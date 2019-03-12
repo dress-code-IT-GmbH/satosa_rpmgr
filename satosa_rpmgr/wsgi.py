2e2b83c42eaf3989e62eb6e88ab2fbc2fc8645d2
rhoerbe <rainer@hoerbe.at>
2019-03-12 19:20:16 +0100

    + gunicorn

--- satosa_rpmgr/wsgi.py
+++ satosa_rpmgr/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for satosa_rpfilter project.
+WSGI config for satosa_rpmgr project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'satosa_rpfilter.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'satosa_rpmgr.settings')
 
 application = get_wsgi_application()
