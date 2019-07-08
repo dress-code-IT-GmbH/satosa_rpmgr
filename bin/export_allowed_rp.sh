d39324accc886e1d5c9888424018565ddd1686cb
rhoerbe <rainer@hoerbe.at>
2019-07-08 17:34:30 +0200

    add export script

---
+++ bin/export_allowed_rp.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+[[ "$APPHOME" ]] || ( echo 'APPHOME not set'; exit 1)
+[[ "$DJANGO_SETTINGS_MODULE" ]] || (echo 'DJANGO_SETTINGS_MODULE not set'; exit 1)
+[[ "$TARGET_ENTITYID" ]] || ( echo 'TARGET_ENTITYID not set'; exit 1)
+
+python $APPHOME/satosa_rpmgr/export_allowed_rp.py --entityid $TARGET_ENTITYID
